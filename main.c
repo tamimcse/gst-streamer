cbefd36e3047b87178e5b2acbf464c963e4e283d
tamimcse <tamim@csebuet.org>
2017-01-18 13:53:42 -0500

    added a sink to test the pipeline

--- main.c
+++ main.c
@@ -39,33 +39,13 @@ bus_call (GstBus     *bus,
 }
 
 
-//static void
-//on_pad_added (GstElement *element,
-//              GstPad     *pad,
-//              gpointer    data)
-//{
-//  GstPad *sinkpad;
-//  GstElement *decoder = (GstElement *) data;
-//
-//  /* We can now link this pad with the vorbis-decoder sink pad */
-//  g_print ("Dynamic pad created, linking demuxer/decoder\n");
-//
-//  sinkpad = gst_element_get_static_pad (decoder, "sink");
-//
-//  gst_pad_link (pad, sinkpad);
-//
-//  gst_object_unref (sinkpad);
-//}
-//
-
-
 int
 main (int   argc,
       char *argv[])
 {
   GMainLoop *loop;
 
-  GstElement *pipeline, *src, *filter, *enc, *svr;
+  GstElement *pipeline, *src, *filter, *enc, *svr, *sink;
   GstBus *bus;
   guint bus_watch_id;
   GstCaps *filtercaps;
@@ -127,32 +107,31 @@ main (int   argc,
     return -1;
   }
   g_object_set (svr, "host", argv[1], NULL);        
-  g_object_set (svr, "port", 8554, NULL);        
-
+  g_object_set (svr, "port", 8554, NULL);    
   
+  sink = gst_element_factory_make ("ximagesink", "sink");
+  if (!sink) {
+    g_printerr ("Cannot create sink.\n");
+    return -1;
+  } 
+
+  /* we add all elements into the pipeline */
+  gst_bin_add_many (GST_BIN (pipeline), src, filter, sink, NULL);
+
+  /* we link the elements together */
+//  gst_element_link (src, filter);
+  if(!gst_element_link_many (src, filter, sink, NULL))
+  {
+    g_printerr ("Cannot link elements.\n");
+    return -1;            
+  }
+
   /* we add a message handler */
   bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
   bus_watch_id = gst_bus_add_watch (bus, bus_call, loop);
   gst_object_unref (bus);
-
-  /* we add all elements into the pipeline */
-  gst_bin_add_many (GST_BIN (pipeline),
-                    src, filter, enc, svr, NULL);
-
-  /* we link the elements together */
-  /* file-source -> ogg-demuxer ~> vorbis-decoder -> converter -> alsa-output */
-  gst_element_link (src, filter);
-  gst_element_link_many (enc, svr, NULL);
-//  g_signal_connect (filter, "pad-added", G_CALLBACK (on_pad_added), enc);
-
-  /* note that the demuxer will be linked to the decoder dynamically.
-     The reason is that Ogg may contain various streams (for example
-     audio and video). The source pad(s) will be created at run time,
-     by the demuxer when it detects the amount and nature of streams.
-     Therefore we connect a callback function which will be executed
-     when the "pad-added" is emitted.*/
-
-
+  
+  
   /* Set the pipeline to "playing" state*/
   g_print ("Now playing: %s\n", argv[1]);
   gst_element_set_state (pipeline, GST_STATE_PLAYING);
